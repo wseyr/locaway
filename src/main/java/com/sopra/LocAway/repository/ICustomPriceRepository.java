544fc7aaaced35ae0b5f98cdf93682b75791f621
flo <prieuflo@gmail.com>
2020-01-09 15:57:46 +0100

    rest user

--- src/main/java/com/sopra/LocAway/repository/ICustomPriceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sopra.LocAway.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sopra.LocAway.model.CustomPrice;
-
-public interface ICustomPriceRepository extends JpaRepository<CustomPrice, Long> {
-
-}
