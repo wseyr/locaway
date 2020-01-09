827a6165fd3c9e2d0a651f895e22540e3bc8896b
GIPAI <jph.deuxguines@gmail.com>
2020-01-09 15:16:34 +0100

    MAJ Mapping tables

--- src/main/java/com/sopra/LocAway/model/CustomPrice.java
+++ src/main/java/com/sopra/LocAway/model/CustomPrice.java
@@ -4,11 +4,11 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.Table;
-
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 
+
+@Entity
 public class CustomPrice {
 	
 	@Id
--- src/main/java/com/sopra/LocAway/model/PointOfInterest.java
+++ src/main/java/com/sopra/LocAway/model/PointOfInterest.java
@@ -4,9 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Table;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
-
+@Entity
 public class PointOfInterest {
 	
 	@Id
