5b62b9455c2f6722351a7ff4e743a1f124e9b5c8
flo <prieuflo@gmail.com>
2020-01-09 14:06:27 +0100

    addUser

---
+++ src/main/java/com/sopra/LocAway/model/User.java
@@ -0,0 +1,11 @@
+package com.sopra.LocAway.model;
+
+public class User {
+	private boolean isAdmin;
+	private String email;
+	private String password;
+	private String phoneNumber;
+	private String firstName;
+	private String lasttName;
+	
+}
