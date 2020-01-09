ff0d96dc0c61865689aa6a362b17446f6f702c0a
orianefont <orianefont@orange.fr>
2020-01-09 15:40:36 +0100

    Mapping

--- src/main/java/com/sopra/LocAway/model/Accomodation.java
+++ src/main/java/com/sopra/LocAway/model/Accomodation.java
@@ -1,6 +1,22 @@
 package com.sopra.LocAway.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Accomodation {
+	@Id
+	@GeneratedValue
+private Long id;
 private String name;
 private String  number;
 private String street;
@@ -10,7 +26,34 @@ private String description;
 private Integer numberOfRooms;
 private Integer maxPersons;
 private boolean isDeleted;
+@Enumerated(EnumType.STRING)
 private EAccomodationType accomodationType;
 private Float defaultBasePrice;
 private Float defaultPersonPrice;
+
+@OneToMany(mappedBy="accomodation")
+private List<CustomPriceDay> customPriceDays;
+
+@OneToMany(mappedBy="accomodation")
+private List<Bookmark> bookmarks;
+
+@OneToMany(mappedBy="accomodation")
+private List<Review> reviews;
+
+@OneToMany(mappedBy="accomodation")
+private List<Photo>photos;
+
+@OneToMany(mappedBy="accomodation")
+private List<Booking> bookings;
+
+@ManyToMany
+private List<Option> options;
+
+@ManyToMany
+private List<PointOfInterest> pointOfInterests;
+
+@ManyToOne 
+private User user;
 }
+
+
--- src/main/java/com/sopra/LocAway/model/Photo.java
+++ src/main/java/com/sopra/LocAway/model/Photo.java
@@ -1,6 +1,22 @@
 package com.sopra.LocAway.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.JoinColumnOrFormula;
+
+@Entity
 public class Photo {
+	@Id
+	@GeneratedValue
+private Long id;
 private String path;
 private boolean isMainPhoto;
+
+@ManyToOne
+@JoinColumn
+private Accomodation accomodation;
 }
