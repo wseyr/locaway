8873b73e7c02e2f0c42f88e6cc2f600ad2407acf
orianefont <orianefont@orange.fr>
2020-01-09 17:01:57 +0100

    début tests accomodation et photo

--- src/main/java/com/sopra/LocAway/model/Accomodation.java
+++ src/main/java/com/sopra/LocAway/model/Accomodation.java
@@ -54,4 +54,296 @@ public class Accomodation {
 
 	@ManyToOne
 	private User user;
+	
+	
+
+	public Accomodation() {
+		super();
+	}
+
+
+
+	public Accomodation(Long id, String name, String number, String street, String city, String country,
+			String description, Integer numberOfRooms, Integer maxPersons, boolean isDeleted,
+			EAccomodationType accomodationType, Float defaultBasePrice, Float defaultPersonPrice,
+			List<CustomPriceDay> customPriceDays, List<Bookmark> bookmarks, List<Review> reviews, List<Photo> photos,
+			List<Booking> bookings, List<Option> options, List<PointOfInterest> pointOfInterests, User user) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.number = number;
+		this.street = street;
+		this.city = city;
+		this.country = country;
+		this.description = description;
+		this.numberOfRooms = numberOfRooms;
+		this.maxPersons = maxPersons;
+		this.isDeleted = isDeleted;
+		this.accomodationType = accomodationType;
+		this.defaultBasePrice = defaultBasePrice;
+		this.defaultPersonPrice = defaultPersonPrice;
+		this.customPriceDays = customPriceDays;
+		this.bookmarks = bookmarks;
+		this.reviews = reviews;
+		this.photos = photos;
+		this.bookings = bookings;
+		this.options = options;
+		this.pointOfInterests = pointOfInterests;
+		this.user = user;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getNumber() {
+		return number;
+	}
+
+
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+
+
+	public String getStreet() {
+		return street;
+	}
+
+
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public Integer getNumberOfRooms() {
+		return numberOfRooms;
+	}
+
+
+
+	public void setNumberOfRooms(Integer numberOfRooms) {
+		this.numberOfRooms = numberOfRooms;
+	}
+
+
+
+	public Integer getMaxPersons() {
+		return maxPersons;
+	}
+
+
+
+	public void setMaxPersons(Integer maxPersons) {
+		this.maxPersons = maxPersons;
+	}
+
+
+
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+
+
+
+	public void setDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+
+
+	public EAccomodationType getAccomodationType() {
+		return accomodationType;
+	}
+
+
+
+	public void setAccomodationType(EAccomodationType accomodationType) {
+		this.accomodationType = accomodationType;
+	}
+
+
+
+	public Float getDefaultBasePrice() {
+		return defaultBasePrice;
+	}
+
+
+
+	public void setDefaultBasePrice(Float defaultBasePrice) {
+		this.defaultBasePrice = defaultBasePrice;
+	}
+
+
+
+	public Float getDefaultPersonPrice() {
+		return defaultPersonPrice;
+	}
+
+
+
+	public void setDefaultPersonPrice(Float defaultPersonPrice) {
+		this.defaultPersonPrice = defaultPersonPrice;
+	}
+
+
+
+	public List<CustomPriceDay> getCustomPriceDays() {
+		return customPriceDays;
+	}
+
+
+
+	public void setCustomPriceDays(List<CustomPriceDay> customPriceDays) {
+		this.customPriceDays = customPriceDays;
+	}
+
+
+
+	public List<Bookmark> getBookmarks() {
+		return bookmarks;
+	}
+
+
+
+	public void setBookmarks(List<Bookmark> bookmarks) {
+		this.bookmarks = bookmarks;
+	}
+
+
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+
+
+	public List<Photo> getPhotos() {
+		return photos;
+	}
+
+
+
+	public void setPhotos(List<Photo> photos) {
+		this.photos = photos;
+	}
+
+
+
+	public List<Booking> getBookings() {
+		return bookings;
+	}
+
+
+
+	public void setBookings(List<Booking> bookings) {
+		this.bookings = bookings;
+	}
+
+
+
+	public List<Option> getOptions() {
+		return options;
+	}
+
+
+
+	public void setOptions(List<Option> options) {
+		this.options = options;
+	}
+
+
+
+	public List<PointOfInterest> getPointOfInterests() {
+		return pointOfInterests;
+	}
+
+
+
+	public void setPointOfInterests(List<PointOfInterest> pointOfInterests) {
+		this.pointOfInterests = pointOfInterests;
+	}
+
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
 }
--- src/main/java/com/sopra/LocAway/model/Photo.java
+++ src/main/java/com/sopra/LocAway/model/Photo.java
@@ -19,4 +19,60 @@ private boolean isMainPhoto;
 @ManyToOne
 @JoinColumn
 private Accomodation accomodation;
+
+
+public Photo() {
+	super();
+}
+
+
+public Photo(Long id, String path, boolean isMainPhoto, Accomodation accomodation) {
+	super();
+	this.id = id;
+	this.path = path;
+	this.isMainPhoto = isMainPhoto;
+	this.accomodation = accomodation;
+}
+
+
+public Long getId() {
+	return id;
+}
+
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+
+public String getPath() {
+	return path;
+}
+
+
+public void setPath(String path) {
+	this.path = path;
+}
+
+
+public boolean isMainPhoto() {
+	return isMainPhoto;
+}
+
+
+public void setMainPhoto(boolean isMainPhoto) {
+	this.isMainPhoto = isMainPhoto;
+}
+
+
+public Accomodation getAccomodation() {
+	return accomodation;
+}
+
+
+public void setAccomodation(Accomodation accomodation) {
+	this.accomodation = accomodation;
+}
+
+
 }
