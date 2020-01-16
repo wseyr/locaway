d6c7c555127a2cb6621b93482bbda5006d56c364
flo <prieuflo@gmail.com>
2020-01-16 10:17:32 +0100

    tout les REST

--- src/main/java/com/sopra/LocAway/repository/IBookedDayRepository.java
+++ src/main/java/com/sopra/LocAway/repository/IBookedDayRepository.java
@@ -2,8 +2,8 @@ package com.sopra.LocAway.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.sopra.LocAway.model.User;
+import com.sopra.LocAway.model.BookedDay;
 
-public interface IBookedDayRepository extends JpaRepository<User, Long> {
+public interface IBookedDayRepository extends JpaRepository<BookedDay, Long> {
 
 }
--- src/main/java/com/sopra/LocAway/repository/IBookingRepository.java
+++ src/main/java/com/sopra/LocAway/repository/IBookingRepository.java
@@ -2,8 +2,8 @@ package com.sopra.LocAway.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.sopra.LocAway.model.BookedDay;
+import com.sopra.LocAway.model.Booking;
 
-public interface IBookingRepository extends JpaRepository<BookedDay, Long> {
+public interface IBookingRepository extends JpaRepository<Booking, Long> {
 
 }
